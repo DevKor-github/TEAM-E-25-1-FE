829b583078bdc85651e5c7d6b14978768db016d7
jiwoo <daisy0928@naver.com>
2025-04-04 16:17:01 +0900

    chore: Tailwind CSS 세팅

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-blue-500 p-6 text-center text-white">
+      <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
