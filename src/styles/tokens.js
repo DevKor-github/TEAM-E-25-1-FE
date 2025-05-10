08e0ba21f7ec2c2ba63dd24082624f67cf5c3214
jiwoo <daisy0928@naver.com>
2025-05-10 22:40:12 +0900

    design: 디자인 토큰 정리

---
+++ src/styles/tokens.js
@@ -0,0 +1,31 @@
+export const typography = {
+  fontFamily: {
+    pretendard: ["Pretendard", "sans-serif"],
+  },
+  fontSize: {
+    heading1: ["56px", { lineHeight: "76px" }],
+    heading2: ["44px", { lineHeight: "60px" }],
+    heading3: ["38px", { lineHeight: "52px" }],
+    title1: ["32px", { lineHeight: "44px" }],
+    title2: ["26px", { lineHeight: "36px" }],
+    title3: ["23px", { lineHeight: "32px" }],
+    title4: ["20px", { lineHeight: "28px" }],
+    body1: ["17px", { lineHeight: "24px" }],
+    body2: ["16px", { lineHeight: "22px" }],
+    body3: ["15px", { lineHeight: "22px" }],
+    tiny1: ["14px", { lineHeight: "20px" }],
+    tiny2: ["13px", { lineHeight: "18px" }],
+    tiny3: ["11px", { lineHeight: "16px" }],
+  },
+};
+
+export const colors = {
+  brand: {
+    sky50: "var(--sky-50, #effaff)",
+    sky100: "var(--sky-100, #def3ff)",
+    sky200: "var(--sky-200, #b6eaff)",
+    sky300: "var(--sky-300, #75dcff)",
+    sky400: "var(--sky-400, #2ccbff)",
+    sky500: "var(--sky-500, #00bbff)", // Univent Sky
+  },
+};
