eb987d67c898a38c37ceb54abcf50650dd8cfd17
jiwoo <daisy0928@naver.com>
2025-06-01 19:53:51 +0900

    상대 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   server: {
     port: 5173,
     host: true,
     headers: {
       "Service-Worker-Allowed": "/",
-      "Access-Control-Allow-Origin": "*"
-    }
+      "Access-Control-Allow-Origin": "*",
+    },
   },
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
-      '@components': resolve(__dirname, 'src/components'),
-      '@pages': resolve(__dirname, 'src/pages'),
-      '@hooks': resolve(__dirname, 'src/hooks'),
-      '@lib': resolve(__dirname, 'src/lib'),
-      '@assets': resolve(__dirname, 'src/assets'),
+      "@components": resolve(__dirname, "src/components"),
+      "@pages": resolve(__dirname, "src/pages"),
+      "@hooks": resolve(__dirname, "src/hooks"),
+      "@lib": resolve(__dirname, "src/lib"),
+      "@assets": resolve(__dirname, "src/assets"),
     },
   },
-})
+});
