b83185b374f546e0aad3491684ee867756e6c959
Junghyun Park <konafish0107@gmail.com>
2025-07-01 20:41:02 +0900

    fix: HeaderFrame 너비 고정

--- src/components/HeaderFrame.tsx
+++ src/components/HeaderFrame.tsx
@@ -7,7 +7,7 @@ const HeaderFrame = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="w-full h-[60px] flex flex-row items-center justify-between border-b border-b-gray-200 px-[20px]">
+    <div className="w-[375px] h-[60px] flex flex-row items-center justify-between border-b border-b-gray-200 px-[20px] mx-auto">
       <img src={logo} alt="univent logo" />
       <div className="flex flex-row items-center gap-4 w-fit h-fit min-w-[64px] min-h-[24px]">
         <img src={heartIcon} alt="scrap" className="cursor-pointer" />
