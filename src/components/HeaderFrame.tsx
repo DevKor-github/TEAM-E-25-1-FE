34f3125eccba5b0dd051fc1032514dee9f742e02
jiwoo <daisy0928@naver.com>
2025-07-01 19:53:59 +0900

    feat: 사용자 페이지 header 유저 아이콘 클릭 시 마이페이지 이동 기능 추가

--- src/components/HeaderFrame.tsx
+++ src/components/HeaderFrame.tsx
@@ -1,14 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import logo from "../assets/logo.svg";
 import heartIcon from "../assets/heartIcon.svg";
 import userIcon from "../assets/userIcon.svg";
 
 const HeaderFrame = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="w-[375px] h-[60px] flex flex-row items-center justify-between border-b border-b-gray-200 px-[20px]">
       <img src={logo} alt="univent logo" />
       <div className="flex flex-row items-center gap-4 w-fit h-fit min-w-[64px] min-h-[24px]">
-        <img src={heartIcon} alt="scrap" />
-        <img src={userIcon} alt="user" />
+        <img src={heartIcon} alt="scrap" className="cursor-pointer" />
+        <img
+          src={userIcon}
+          alt="user"
+          className="cursor-pointer"
+          onClick={() => navigate("/user")}
+        />
       </div>
     </div>
   );
