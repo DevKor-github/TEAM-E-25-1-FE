b6c2d7d0f4d2797e2722f606d9bf5fa76c994586
jiwoo <daisy0928@naver.com>
2025-07-06 21:11:17 +0900

    chore: axios baseURL 환경변수로 설정

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
 export const api = axios.create({
-  baseURL: "https://goyangeyaong.shop/",
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
