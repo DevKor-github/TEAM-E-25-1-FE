e04603b1ddb9c74bb43de88f55f7f411a29448e0
Junghyun Park <konafish0107@gmail.com>
2026-01-15 23:01:04 +0900

    chore: 사용하지 않는 import 제거

--- src/components/OrgInfoSheet.tsx
+++ src/components/OrgInfoSheet.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { api } from "@/lib/axios";
 import { useOrgAuthStore } from "@/stores/orgAuthStore";
 import { Button } from "@/components/ui/button";
